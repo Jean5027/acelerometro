3720d04e2ab2d024c509eade0d3e002b3b702eb0
Jean5027 <jeanviana5027@gmail.com>
2023-04-20 14:43:11 -0300

    primeiro commit

---
+++ src/screens/Post/styles.ts
@@ -0,0 +1,20 @@
+import { StyleSheet } from "react-native"; 
+
+
+export const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        justifyContent: 'center',
+        alignItems: 'center',
+       
+       
+
+    },
+    title: {
+        fontSize: 31,
+        color: '#fff',
+        fontWeight: 'bold',
+
+    }
+});
